317c431ecf2850e508b3dc97ac61947ce03dc9e1
Luke <lwheeler9@gmail.com>
2025-06-06 15:16:26 -0600

    update parse_dataset function to be more flexible with regard to sub-directory structure in processed data directories - rename to parse_proc_dataset

--- dispa/__init__.py
+++ dispa/__init__.py
@@ -1,4 +1,4 @@
-from .utils import load_pdata, parse_dataset
+from .utils import load_pdata, parse_proc_dataset
 from .calculations import get_ppm_scale, get_ppm_scale_manual, rotate, magnitude_transformation, calc_snr
 from .plots import polar_plot, panel_plot, overlay_plot
 from .algorithms import optimize_rotation_rms_file, optimize_rotation_rms_mem, optimize_rotation_rms_NoFiles, find_saddle
