00f192c92ae37895846729bfc6ac8bf3e463a391
Luke <lwheeler9@gmail.com>
2025-07-10 17:38:14 -0600

    add the estimate_separation_error function on a new branch

--- dispa/algorithms.py
+++ dispa/algorithms.py
@@ -1,9 +1,12 @@
 from dispa import load_pdata, get_ppm_scale, get_ppm_scale_manual, magnitude_transformation, rotate
+from dispa import fidgen, phaseshift, specgen, addnoise, fidcomb, write_fid_TS
 from scipy.interpolate import interp1d
 from scipy.signal import argrelmin
 import numpy as np
 import matplotlib.pyplot as plt
-
+import os
+import nmrglue as ng
+import pandas as pd
 
 def find_saddle(params, data, ppm_region, plot=False, plotname="saddle-plot"):
     """Function to identify local saddle points between peaks in 1D NMR data.
@@ -628,3 +631,271 @@ def optimize_rotation_rms_NoFiles(spec1, spec2, ppm_region, step_deg=1, plot=Tru
 
         
     return best_angle, min_rms, angles_deg, rms_values, data_rotated, rms_values_init
+  
+    
+def estimate_separation_error(focal_width, width2, focal_intensity, intensity2, focal_center, center2, parameters, output_folder, theta_ps, plot=True):
+    """Function to estimate the error in phase-shift estimates caused by peak overlap via simulation.
+    
+    Parameters
+
+    ----------
+
+    focal_width: float
+        Width in Hz of focal peak
+    width2: float
+        Width in Hz of non-focal peak
+    focal_intensity: float
+        Intensity of focal peak
+    intensity2: float
+        Intensity of non-focal peak
+    focal_center: float
+        Center (in Hz) of focal peak
+    center2: float
+        Center (in Hz) of non-focal peak
+    parameters: dict
+        Dictionary containing simulation parameters (e.g. {"n":4096, "dw":0.001, "bf":100, "SW_p":1000, "OFFSET":5, "nc_proc":11})
+    output_folder: str
+        name of folder to store outputs
+    theta_ps: float
+        Phase shift angle for focal peak (degrees)
+    plot: bool
+        Whether or not to generate and save optional plots of spectra and ROI error curves
+        
+    Returns
+    
+    -------
+    
+    expansions_p0 : pandas.DataFrame
+        Dataframe containing estimates and errors for non-focal peak
+    expansions_p10 : pandas.DataFrame
+        Dataframe containing estimates and errors for focal phase-shifted peak
+    min_err_focal : float
+        Minimum ROI error calculated from the ROI scan for focal peak
+    min_err_nops : float
+        Minimum ROI error calculated from the ROI scan for non-focal peak     
+    """
+    
+    ### Simulate comparable no-noise spectra based on user-input parameters
+    
+    # Make sure the user specifies focal_center as the peak of interest
+    
+    theta_noshift = 0         # phase shift to apply to first peak
+    theta_shift = theta_ps         # phase shift to apply to second peak
+
+    # need to focus on the peak of interest that might have phase shift (width1)
+    
+    # Get ratio of largest to smallest peak intensities for scaling simulations
+    intensities = [focal_intensity, intensity2]
+    intensity_ratio = max(intensities)/min(intensities)
+    
+    # Automatically identify a relevant range of peak separations
+    centers = [focal_center, center2]
+    
+    # Create descriptive expnos for write function to save files
+    expno = "FocalPeakShift" + str(focal_center) + "_" + "FocalPeakWidth" + str(focal_width) + "_" + "PhaseShift" + "0deg"
+    expno_ps = "FocalPeakShift" + str(focal_center) + "_" + "FocalPeakWidth" + str(focal_width) + "_" + "PhaseShift" + str(theta_shift)+ "deg"
+    
+    ### Simulate data for peaks with phase shift ### 
+
+    # Generate both peaks
+    fid1 = fidgen(focal_center, focal_width, parameters["n"], parameters["dw"]) #this is the focal peak, which will have phase shift
+    fid2 = fidgen(center2, width2, parameters["n"], parameters["dw"])
+        
+    # Remove DC
+    fid1[0] = fid1[0]/2
+    fid2[0] = fid2[0]/2
+
+    # alter the intensity of the second peak
+    if focal_intensity == max(intensities):
+        fid1 = fid1*intensity_ratio
+    elif intensity2 == max(intensities):
+        fid2 = fid2*intensity_ratio
+        
+    # Apply phase shift
+    fid1 = phaseshift(fid1, parameters["dw"], theta_shift)
+    fid2 = phaseshift(fid2, parameters["dw"], theta_noshift)
+
+    # Combine the fids - do not add noise (assume max possible SNR)
+    fid = fid1 + fid2
+
+    # Assign folder path for this experiment
+    foldername = output_folder +"/" + str(expno_ps)
+    
+    # Write to TopSpin format
+    write_fid_TS(fid, parameters["dw"], parameters["bf"], foldername)
+    
+    
+    ### Simulate data for peaks without phase shift ###
+    
+    # Generate both peaks
+    fid3 = fidgen(focal_center, focal_width, parameters["n"], parameters["dw"]) #this is the focal peak, but without phase shift
+    fid4 = fidgen(center2, width2, parameters["n"], parameters["dw"])
+        
+    # Remove DC
+    fid3[0] = fid3[0]/2
+    fid4[0] = fid4[0]/2
+
+    # alter the intensity of the second peak
+    if focal_intensity == max(intensities):
+        fid3 = fid3*intensity_ratio
+    elif intensity2 == max(intensities):
+        fid4 = fid4*intensity_ratio
+        
+    # Apply phase shift
+    fid3 = phaseshift(fid3, parameters["dw"], theta_noshift)
+    fid4 = phaseshift(fid4, parameters["dw"], theta_noshift)
+
+    # Combine the fids - do not add noise (assume max possible SNR)
+    fid_ns = fid3 + fid4
+
+    # Assign folder path for this experiment
+    foldername = output_folder +"/" + str(expno)
+    
+    # Write to TopSpin format
+    write_fid_TS(fid_ns, parameters["dw"], parameters["bf"], foldername)
+
+
+    # Load the simualate spectra back from files into memory - store in experiments dictionary
+    experiments = {}
+
+    for d in os.listdir(output_folder):
+        #if os.path.isdir(os.path.join(d, output_folder)):
+        meta, data = ng.bruker.read(output_folder+"/"+d, "fid")
+        fid_fft, f_fft = specgen(data, dw=parameters["dw"])
+        test_data = np.array([fid_fft.real, fid_fft.imag])
+    
+        meta['procs']['OFFSET'] = parameters["OFFSET"] #this is a kludge to deal with missing parameter in procs for now
+        meta['procs']['SW_p'] = parameters["SW_p"] #this is a kludge to deal with missing parameter in procs for now
+    
+        experiments[d] = [meta, test_data]
+    
+    # Generate an array of seed ROIs for each spectrum based on input peak centers
+    seed_roi_p10 = [focal_center/parameters["bf"]-0.005, focal_center/parameters["bf"]+0.005]
+    seed_roi_p0 = [center2/parameters["bf"]-0.005, center2/parameters["bf"]+0.005]
+
+
+    ### Walk outward from seed ROIs and estimate phase shift ###
+
+    # Assign test datasets to new variables with simple labels
+    test_data_0 = experiments["FocalPeakShift" + str(focal_center) + "_" + "FocalPeakWidth" + str(focal_width) + "_" + "PhaseShift" + "0deg"][1]
+    test_data_10 = experiments["FocalPeakShift" + str(focal_center) + "_" + "FocalPeakWidth" + str(focal_width) + "_" + "PhaseShift" + str(theta_shift)+ "deg"][1]
+
+    # Set up number of iterations and increment for ROI walkout
+    num_iter = 100
+    increment = 0.005
+    
+    # Set up dictionaries to hold angle estimates
+    expansions_p0 = {}
+    expansions_p10 = {}
+
+    
+    # Initial estimate point with seed roi
+    best_angle_p0, min_rms_p0, angles_deg_p0, rms_values_p0, data_rotated_p0, rms_values_init_p0 = optimize_rotation_rms_NoFiles(spec1=test_data_0, spec2=test_data_10, 
+                                                                                                                                 ppm_region=seed_roi_p0,step_deg=0.001,
+                                                                                                                                 plot=False, 
+                                                                                                                                 plotname="NULL0", figsize=(12,4), 
+                                                                                                                                 nc_proc=parameters["nc_proc"], 
+                                                                                                                                 offset_ppm=parameters["OFFSET"], 
+                                                                                                                                 dw=parameters["dw"])   
+
+    
+    best_angle_p10, min_rms_p10, angles_deg_p10, rms_values_p10, data_rotated_p10, rms_values_init_p10 = optimize_rotation_rms_NoFiles(spec1=test_data_0, spec2=test_data_10, 
+                                                                                                                                       ppm_region=seed_roi_p10,step_deg=0.001, 
+                                                                                                                                       plot=False, 
+                                                                                                                                       plotname="NULL10", figsize=(12,4),
+                                                                                                                                       nc_proc=parameters["nc_proc"], 
+                                                                                                                                       offset_ppm=parameters["OFFSET"], 
+                                                                                                                                       dw=parameters["dw"])   
+
+    
+    expansions_p0[tuple(seed_roi_p0)] = best_angle_p0
+    expansions_p10[tuple(seed_roi_p10)] = best_angle_p10
+    
+    
+    # Iterate over the seed ROI walkout to estimate errors
+    for j in range(0, num_iter):
+    
+        seed_roi_p0[0] -= increment
+        seed_roi_p0[1] += increment
+
+        seed_roi_p10[0] -= increment
+        seed_roi_p10[1] += increment
+        
+        best_angle_p0, min_rms_p0, angles_deg_p0, rms_values_p0, data_rotated_p0, rms_values_init_p0 = optimize_rotation_rms_NoFiles(spec1=test_data_0, spec2=test_data_10, 
+                                                                                                                                     ppm_region=seed_roi_p0, step_deg=0.001,
+                                                                                                                                     plot=False, 
+                                                                                                                                     plotname="NULL0", figsize=(12,4),
+                                                                                                                                     nc_proc=parameters["nc_proc"], 
+                                                                                                                                     offset_ppm=parameters["OFFSET"], 
+                                                                                                                                     dw=parameters["dw"])    
+        
+        best_angle_p10, min_rms_p10, angles_deg_p10, rms_values_p10, data_rotated_p10, rms_values_init_p10 = optimize_rotation_rms_NoFiles(spec1=test_data_0, spec2=test_data_10, 
+                                                                                                                                           ppm_region=seed_roi_p10, step_deg=0.001, 
+                                                                                                                                           plot=False, 
+                                                                                                                                           plotname="NULL10", figsize=(12,4),
+                                                                                                                                           nc_proc=parameters["nc_proc"], 
+                                                                                                                                           offset_ppm=parameters["OFFSET"], 
+                                                                                                                                           dw=parameters["dw"])     
+    
+        expansions_p0[tuple(seed_roi_p0)] = best_angle_p0
+        expansions_p10[tuple(seed_roi_p10)] = best_angle_p10
+
+        
+    # convert dictionaries to pd.DataFrame
+    expansions_p0 = pd.DataFrame.from_dict(expansions_p0, orient="index").sort_index()
+    expansions_p10 = pd.DataFrame.from_dict(expansions_p10, orient="index").sort_index()
+
+    expansions_p0["width"] = [i[1] - i[0] for i in expansions_p0.index]
+    expansions_p10["width"] = [i[1] - i[0] for i in expansions_p10.index]
+
+    expansions_p0.columns = ["estimate", "width"]
+    expansions_p10.columns = ["estimate", "width"]
+
+
+    expansions_p0["AbsErr"] = theta_noshift - expansions_p0["estimate"]
+    expansions_p10["AbsErr"] = theta_shift - expansions_p10["estimate"]
+
+    # Get min errors from the tables
+    min_err_focal = np.min(expansions_p10["AbsErr"])
+    min_err_nops = np.min(expansions_p0["AbsErr"])
+
+
+    if plot == True:
+        ## Plot the simulated data ##
+        ppm = get_ppm_scale_manual(offset_ppm=parameters["OFFSET"], sw_Hz=1/parameters["dw"], sf_MHz=parameters["bf"], si=len(test_data_0[0]))
+        
+        plt.plot(ppm, test_data_0[0], color="blue", alpha=0.8)
+        plt.plot(ppm, test_data_10[0], color="red", alpha=0.8)
+        plt.xlim(max([focal_center, center2])/parameters["bf"]+0.25, min([focal_center, center2])/parameters["bf"]-0.25)
+        
+        plt.legend({'No Phase Shift':"blue", '{} Phase Shift'.format(theta_shift):"red"})
+        plt.xlabel("PPM")
+        plt.ylabel("a.u."); 
+        
+        plt.axvline(focal_center/parameters["bf"], ls="--", color="black", lw=1)
+        plt.axvline(center2/parameters["bf"], ls="--", color="black", lw=1)
+        
+        plt.savefig(output_folder + "/simulated-twopeak-spectra-{}.png".format(theta_shift), dpi=300)
+        plt.show()
+    
+        
+        ## Plot ROI error curves ##
+        plt.scatter(np.log10(expansions_p10["width"]), expansions_p10["estimate"], s=5, color="red")
+        plt.scatter(np.log10(expansions_p0["width"]), expansions_p0["estimate"], s=5, color="blue")
+        
+        plt.axhline(theta_shift, ls="--", lw=1, color="red")
+        #plt.text(0.8*min(np.log10(expansions_p10["width"])), theta_shift, "True Phase Shift", color="red")
+        
+        plt.axhline(0, ls="--", lw=1, color="blue")
+        #plt.text(0.8*min(np.log10(expansions_p0["width"])), theta_noshift, "True Phase Shift", color="blue")
+        
+        plt.legend({"{} PS".format(theta_shift):"red", "No PS":"blue"})
+        plt.title("ROI vs Angle Estimate Curves (0 and {}deg PS)".format(theta_shift), fontsize=11)
+        
+        plt.xlabel("log$_{10}$(ROI width (ppm))")
+        plt.ylabel("Estimated Rotation (degrees)");
+        
+        plt.savefig(output_folder + "/ROI-vs-BestAngle-2Peak-curves-log-{}.png".format(theta_shift), dpi=300)
+        plt.show()
+
+    return expansions_p0, expansions_p10, min_err_focal, min_err_nops
